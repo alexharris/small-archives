3c8b85f98693b147dfd3c4c078938d1ceaba8ffb
alexharris <alexharris6@gmail.com>
2020-03-23 12:02:11 -0700

    add fathom

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,8 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '~/plugins/firebase.js'
+    '~/plugins/firebase.js',
+    '~/plugins/fathom.js'
   ],
   /*
   ** Nuxt.js dev-modules
